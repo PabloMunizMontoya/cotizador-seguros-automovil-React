1eed6b54db167e768e58b204850e0a7f1c952328
PabloMunizMontoya <119509495+PabloMunizMontoya@users.noreply.github.com>
2023-03-14 18:54:21 -0500

    agregamos el contenedor del formulario con styledComponents

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
 import React from 'react'
 import Header from './components/Header';
 
-//2.importamos style components
-/* import styled from  */
+//3.importamos style components
+import styled from '@emotion/styled'
 
-//2.1 creamos el contenedor y le damos los estilos
-/* const Contenedor = styled.div`
+//3.1 creamos el contenedor y le damos los estilos
+const Contenedor = styled.div`
   max-width: 600px;
   margin: 0 auto;
   
-` */
+` 
 
 function App() {
   return (
-    //2.2 insertamos el componente con su estilo y adentro ponemos el header
-    {/* <Contenedor> */}
-     /*  <Header
+    //3.2 insertamos el componente con su estilo y adentro ponemos el header
+    <Contenedor> 
+      <Header
         titulo = 'Cotizador de Seguros'
-      /> */
-    /* </Contenedor> */
+      /> 
+    </Contenedor>
   );
 }
 
