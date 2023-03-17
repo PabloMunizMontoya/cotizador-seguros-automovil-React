38f7260ee5daf61aa2ec18c174ed71346e0f3245
PabloMunizMontoya <119509495+PabloMunizMontoya@users.noreply.github.com>
2023-03-16 22:38:43 -0500

    agregamos una trasicion al resultado del valor con la libreria transitionGroup

--- src/components/Resultado.jsx
+++ src/components/Resultado.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import styled from '@emotion/styled'
+//27. importamos transitionGroup, librería para hacer transiciones.
+import  { TransitionGroup, CSSTransition } from 'react-transition-group' 
 
 //25. creamos es styledComponent para darle estilo al menaje del total de la cotizacion.
 const MensajeCotizacion = styled.p`
@@ -24,13 +26,30 @@ const Resultado = ({cotizacion}) => {
     return ( 
         /* 26.1 insertamos el styledComponent div */
         <ResultadoCotizacion>
-             {/* 25.2 cambiamos el <p> por el component styled  */}
-            <MensajeCotizacion>El       total es : $ 
-             {/* 22.4 renderizado el valor de la prop traída desde app  */}
-            {cotizacion}
-            </MensajeCotizacion>
+            {/* 27.1 insertamos los componentes de la librería de transición, estos van a tener unas props, componente le indica a que componente se le hace la transition, como en este caso MensajeDeCotizacion es un p componente tiene valor de p, y className hace referencia a un className en la hoja de estilos css */}
+            <TransitionGroup
+                component='p'
+                className='resultado'
+            >
+                {/* 27.2 esta es la sintaxis para la librería de transiciones nótese que aca es classNames, el key es a que valor se le va a agregar la transición, y el time out es el tiempo de salida */} 
+                <CSSTransition
+                    classNames='resultado'
+                    key= {cotizacion}
+                    timeout={{enter: 500, exit: 500}}
+                >   
+                
+                    {/* 25.2 cambiamos el <p> por el component styled  */}
+                    <MensajeCotizacion>
+                        
+                        {/* 22.4 renderizado el valor de la prop traída desde app  */}
+                        El total es : $ {cotizacion}
+                    </MensajeCotizacion>
+
+                </CSSTransition>
+
+            </TransitionGroup>
+
         </ResultadoCotizacion>
-        
     );
 }
 
